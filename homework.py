a6a4f50d6de71d4f771e9bdc6420cfb68eba0995
r3t4rdme <72883128+r3t4rdme@users.noreply.github.com>
2021-08-29 22:37:06 +0300

    first push

--- homework.py
+++ homework.py
@@ -1,51 +1,88 @@
+import logging
 import os
 import time
+from logging.handlers import RotatingFileHandler
+
 import requests
 import telegram
 from dotenv import load_dotenv
-
 
 load_dotenv()
 
 PRAKTIKUM_TOKEN = os.getenv('PRAKTIKUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
-CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-# проинициализируйте бота здесь,
-# чтобы он был доступен в каждом нижеобъявленном методе,
-# и не нужно было прокидывать его в каждый вызов
-bot = ...
+current_timestamp = int(time.time())
+
+bot = telegram.Bot(token=TELEGRAM_TOKEN)
+
+url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+
+logging.basicConfig(level=logging.DEBUG,
+                    filename='program.log',
+                    format='%(asctime)s, %(levelname)s, %(message)s, %(name)s')
+
+# а тут настраиваем логгер для текущего файла .py
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = RotatingFileHandler(
+    'my_logger.log', maxBytes=50000000, backupCount=5)
+logger.addHandler(handler)
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
-        verdict = 'К сожалению, в работе нашлись ошибки.'
-    else:
-        verdict = 'Ревьюеру всё понравилось, работа зачтена!'
-    return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    homework_name = homework.get('homework_name')
+    homework_status = homework.get('status')
+    homework_statuses = {
+        'approved': 'Ревьюеру всё понравилось, работа зачтена!',
+        'rejected': 'К сожалению, в работе нашлись ошибки.',
+        'reviewing': 'Проект на стадии ревью'
+    }
+    if homework_name is None:
+        logging.eror('Работа не найдена')
+        return 'Работа не найдена'
+    if homework_status in homework_statuses:
+        verdict = homework_statuses[homework_status]
+        return f'У вас проверили работу "{homework_name}"!\n\n{verdict}'
+    logging.error('Неизвестный статус')
+    return 'Неизвестный статус'
 
 
 def get_homeworks(current_timestamp):
-    homework_statuses = ...
+    headers = {'Authorization': f'OAuth {PRAKTIKUM_TOKEN}'}
+    payload = {'from_date': current_timestamp}
+    try:
+        homework_statuses = requests.get(url, headers=headers, params=payload)
+    except requests.exceptions.RequestException as e:
+        raise f'Ошибка при запросе сервера Практикума: {e}'
     return homework_statuses.json()
 
 
 def send_message(message):
-    return bot.send_message(...)
+    return bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=message)
 
 
 def main():
     current_timestamp = int(time.time())  # Начальное значение timestamp
+    seconds_in_month = (60 * 60 * 24 * 30)
+    time_brackets = current_timestamp - seconds_in_month
 
     while True:
         try:
-            ...
-            time.sleep(5 * 60)  # Опрашивать раз в пять минут
-
+            logger.debug('Бот начал работу.')
+            send_message(
+                parse_homework_status(
+                    get_homeworks(time_brackets)
+                ))
+            logger.info('Бот отправил сообщение.')
+            time.sleep(5 * 5)  # Опрашивать раз в пять минут
         except Exception as e:
-            print(f'Бот упал с ошибкой: {e}')
+            error_message = f'Бот упал с ошибкой: {e}'
+            logger.error(error_message)
+            bot.send_message(chat_id=TELEGRAM_CHAT_ID, text=error_message)
             time.sleep(5)
+
 
 
 if __name__ == '__main__':
